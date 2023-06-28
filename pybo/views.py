3a3fcff8140fb3779f89d960e25f3af694399338
zongseo <natarzan99@gmail.com>
2023-06-28 17:28:23 +0900

    feat: create navigationbar and paging

--- pybo/views.py
+++ pybo/views.py
@@ -4,12 +4,16 @@ from django.http import HttpResponseNotAllowed
 
 from .models import Question
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
